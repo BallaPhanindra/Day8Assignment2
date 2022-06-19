0f639ba1f5c09ea0b277f452297c0639b4a06390
Balla <ballaphanindra@outlook.com>
2022-06-19 17:05:38 +0530

    initial commit

---
+++ Day8Assignment2/Day8Assignment2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day8Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("=======UC1=========");
+            UC1 u1 = new UC1();
+            u1.length();
+
+            Console.WriteLine("=======UC2=========");
+            UC2 u2 = new UC2();
+            u2.ComparingLines();
+
+            UC3 u3 = new UC3();
+            u3.Comparing();
+        }
+    }
+}
---
+++ Day8Assignment2/Day8Assignment2/UC3.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8Assignment2
+{
+    internal class UC3
+    {
+        public void Comparing()
+        { 
+        int x1 = 2;
+        int y1 = 3;
+
+        int x2 = 4;
+        int y2 = 1;
+
+        int x11 = 3;
+        int y11 = 4;
+
+        int x12 = 4;
+        int y12 = 5;
+
+        int x = (x2 - x1) * (x2 - x1);
+        int y = (y2 - y1) * (y2 - y1);
+        int z = x + y;
+       
+          
+        Console.WriteLine("length of 1st line: " +Math.Sqrt(z));
+
+        int u = (x11 - x12) * (x11 - x12);
+        int v = (y11 - y12) * (y11 - y12);
+        int w = u + v;
+
+
+        Console.WriteLine("length of the 2nd line: " + Math.Sqrt(w));
+
+
+            if (Math.Sqrt(z).CompareTo(Math.Sqrt(w)) == 1)
+            {
+                Console.WriteLine("1st line is greater than 2nd line");
+            }
+            else if((Math.Sqrt(z).CompareTo(Math.Sqrt(w)) == 1))
+            {
+                Console.WriteLine("2nd one is greater than 1st line");
+            }
+            else
+            {
+                Console.WriteLine("Both lines are equal");
+            }
+
+
+        }
+    }
+}
